ed5058d7df8bffe1671350bef709237cf35e539c
shn <sreeharinagesh@gmail.com>
2026-04-18 08:26:38 +0000

    added comments.

--- queue.go
+++ queue.go
@@ -10,6 +10,8 @@ func NewQueue() *Queue {
 	return &Queue{jobs: make(chan Job)}
 }
 
+// AddTask enqueues Task to the queue (Job channel)
+// a UUID is generated per unique job
 func (q *Queue) AddTask(task Task) string {
 	id := uuid.NewString()
 
--- task.go
+++ task.go
@@ -3,16 +3,20 @@ package bhootam
 type Args []any
 type Func func(Args) Outcome
 
+// Outcome is the return value we expect from a function
 type Outcome struct {
 	Value any
 	Err   error
 }
 
+// Task is an abstraction layer
+// We expect the user functions to be wrapped inside Task
 type Task struct {
 	Function Func
 	Args     Args
 }
 
+// Run executes the function with the provided arguments
 func (t Task) Run() Outcome {
 	return t.Function(t.Args)
 }
